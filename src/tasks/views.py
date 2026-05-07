1366c22c89b6fa9ee2bad3c22e95c9949edd1000
Swetamdhr <cihe240378@student.cihe.edu.au>
2026-05-07 16:27:59 +1000

    Add RBAC - separate dashboard for admin and regular users

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_staff:
+        return render(request, 'admin_home.html')
+    else:
+        return render(request, 'user_home.html')
